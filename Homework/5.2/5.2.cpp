85af6842fb076ada8501c144b176d964e3fcd547
Thirawat <teerawattumakul@gmail.com>
2025-10-12 22:57:29 +0700

    Create 5.2.cpp

---
+++ Homework/5.2/5.2.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int score;
+
+    printf("Please enter your score: ");
+    if (scanf("%d", &score) != 1) { 
+        printf("please enter number only.\n");
+        return 0;
+    }
+
+    if (score >= 80) {
+        printf("A!\n");
+    }
+    else if (score >= 75) {
+        printf("B+!\n");
+    }
+    else if (score >= 70) {
+        printf("B!\n");
+    }
+    else if (score >= 65) {
+        printf("C+!\n");
+    }
+    else if (score >= 60) {
+        printf("C!\n");
+    }
+    else if (score >= 55) {
+        printf("D+!\n");
+    }
+    else if (score >= 50) {
+        printf("D!\n");
+    }
+    else {
+        printf("F!\n");
+    }
+
+    return 0;
+}
