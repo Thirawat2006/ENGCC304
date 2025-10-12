f2ed0dda8d825dae75d8f7c599c67bf47dd99804
Thirawat <teerawattumakul@gmail.com>
2025-10-12 22:29:18 +0700

    Update 3.1.cpp

--- Homework/3.1/3.1.cpp
+++ Homework/3.1/3.1.cpp
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    float h, w;
+
+    printf("Enter H = ");
+    scanf("%f", &h);
+
+    printf("Enter W = ");
+    scanf("%f", &w);
+
+    printf("%.1f\n", (h * w) / 2);
+    return 0;
+}
